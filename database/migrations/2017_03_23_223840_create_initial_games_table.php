228684b77226e024a610a361ab09798aa8b65d8b
Seth Wright <asethwright@gmail.com>
2017-03-28 13:16:17 -0400

    Initial commit.

---
+++ database/migrations/2017_03_23_223840_create_initial_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialGamesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('games', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('home');
+      $table->string('away');
+      $table->integer('ptsHome');
+      $table->integer('ptsAway');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('games');
+  }
+}
